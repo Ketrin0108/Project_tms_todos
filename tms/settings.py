bfc0f5384f6f84256e8fe5e21097e56e09af643c
Ketrin0108 <k-ketrin@list.ru>
2023-07-05 01:39:04 +0300

    Add swagger documentation,env.template,static

--- tms/settings.py
+++ tms/settings.py
@@ -9,8 +9,12 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
+import os
 from datetime import timedelta
 from pathlib import Path
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,10 +25,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ov#0rq(y=x7zq6x^59zvj$^(cbb6l!th+a=tf*8hhrggxjw52f'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+
+DEBUG = os.environ.get('DEBUG')
 
 ALLOWED_HOSTS = []
 
@@ -46,6 +51,8 @@ INSTALLED_APPS = [
     'rest_framework.authtoken',
     'rest_framework_simplejwt',
     'debug_toolbar',
+    'drf_yasg',
+    'coreapi',
 ]
 
 MIDDLEWARE = [
@@ -94,10 +101,12 @@ REST_FRAMEWORK = {
     'DEFAULT_CACHE_BACKEND': 'default',
 }
 
+SUPPORT_TEAM_EMAIL=os.environ.get('SUPPORT_TEAM_EMAIL')
+
 CACHES = {
     "default": {
         "BACKEND": "django_redis.cache.RedisCache",
-        "LOCATION": "redis://127.0.0.1:6379/1",
+        "LOCATION": os.environ.get('REDIS_URL'),
         "OPTIONS": {
             "CLIENT_CLASS": "django_redis.client.DefaultClient",
         }
@@ -120,7 +129,7 @@ WSGI_APPLICATION = 'tms.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': os.environ.get('DATABASE_NAME'),
     }
 }
 
@@ -191,3 +200,5 @@ def show_toolbar(request):
 DEBUG_TOOLBAR_CONFIG = {
     'SHOW_TOOLBAR_CALLBACK': 'tms.settings.show_toolbar',
 }
+
+STATIC_ROOT = os.path.join(BASE_DIR,'static')
--- tms/urls.py
+++ tms/urls.py
@@ -17,7 +17,27 @@ Including another URLconf
 from django.contrib import admin
 from django.conf import settings
 from django.urls import path, include
+from django.contrib import admin
+from django.urls import include
+from django.urls import path
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
+from rest_framework import permissions
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Todos API Documentation",
+        default_version="v1",
+        description="API documentation for social network app",
+        terms_of_service="https://www.example.com/terms/",
+        contact=openapi.Contact(email=settings.SUPPORT_TEAM_EMAIL),
+        license=openapi.License(name="BSD License"),
+    ),
+    public=True,
+    permission_classes=(permissions.IsAuthenticated,),
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -26,6 +46,8 @@ urlpatterns = [
     path('api/v2/', include('todos.urls.api_v2')),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh', TokenRefreshView.as_view(), name='token_refresh'),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 
 ]
 
